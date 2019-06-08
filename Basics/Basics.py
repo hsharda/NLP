6e6425857b624e5ed3ee1152de983eaba518e965
Harsh Sharda <hs957@georgetown.edu>
2019-06-08 21:41:54 +0300

    using reg-ex

--- Basics/Basics.py
+++ Basics/Basics.py
@@ -58,3 +58,46 @@ print(f'''The data has {len(completedata_pd)} rows and {len(completedata_pd.colu
 
 ham_spam = completedata_pd.groupby(completedata_pd.label).count()
 
+## How much missing data there is
+
+print(completedata_pd.isnull().sum())
+
+
+## Using Regular Expression examples to get a hang of how it works
+
+import re
+
+
+# Test examples: When
+## Case1: Dealing with characters which split a word
+re_test = 'This is a made up string to test 2 different regex methods'
+re_test_messy = 'This      is a made up     string to test 2    different regex methods'
+re_test_messy1 = 'This-is-a-made/up.string*to>>>>test----2""""""different~regex-methods'
+
+# Splitting the sentence into a list of words
+
+# When dealing with single spaced
+re.split("\s",re_test)
+
+# When dealing with more than just one space
+re.split("\s+",re_test_messy)
+
+# When dealing with punctuation marks
+re.split("\W+",re_test_messy1)
+
+## Case2: Finding characters which we don't need
+
+# When dealing with a single spaced or more
+re.findall("\S+",re_test)
+re.findall("\S+",re_test_messy)
+
+# When dealing with punctuation marks
+re.findall("\w+",re_test_messy1)
+
+
+# Test examples: Replacing the string
+
+pep8_test = 'I try to follow PEP8 guidelines'
+pep7_test = 'I try to follow PEP7 guidelines'
+peep8_test = 'I try to follow PEEP8 guidelines'
+
