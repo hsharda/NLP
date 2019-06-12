a5c14e90a1886aa0ea24021a37114965845dd7c2
Harsh Sharda <hs957@georgetown.edu>
2019-06-12 21:09:59 +0300

    adding stopwords function

--- Basics/pre_processing.py
+++ Basics/pre_processing.py
@@ -9,25 +9,36 @@ Created on Sun Jun  9 23:13:08 2019
 # The aim is to understand what goes under the hood of pre-processing text data
 
 import pandas as pd
+import string # To remove punctuations
+import re # For tokenization
+import nltk # For removing stopwords 
 
+# Reading csv and creating variable names
 completedata_pd = pd.read_csv('../data/SMSSpamCollection.tsv', sep = "\t", header = None)
 completedata_pd.columns = ['label','text']
 
-import string # To remove punctuations
-import re # For tokenization
-
 # Building a function to remove punctuations
 
-def remove(x):
+def remove(text):
     for c in string.punctuation:
-        x=x.replace(c,"")
-    return x
+        text=text.replace(c,"")
+    return text
 
 completedata_pd['clean_text']= completedata_pd['text'].apply(remove)
 
 # Building a function to tokenize
 
-def tokenize(x):
-    return re.split('\W+',x)
+def tokenize(clean_text):
+    return re.split('\W+',clean_text)
 
 completedata_pd['clean_text_tokens']= completedata_pd['clean_text'].apply(lambda x: tokenize(x.lower()))
+
+# Building a function to remove stopwords
+
+stopwords = nltk.corpus.stopwords.words('english')
+
+def remove_stopwords(tokenize_col):
+    stop = [words for words in tokenize_col if words not in stopwords]
+    return stop
+
+completedata_pd['final_clean_text']= completedata_pd['clean_text_tokens'].apply(lambda x: remove_stopwords(x))
